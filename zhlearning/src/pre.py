bab36651808e5de49115ab9fe17f2c797ed83845
muyun <wenlzhao@gmail.com>
2022-05-23 11:53:46 +0800

    add

--- zhlearning/src/pre.py
+++ zhlearning/src/pre.py
@@ -1,14 +1,17 @@
 import spacy
 from spacy.lang.zh.examples import sentences
 
+nlp = spacy.load("zh_core_web_sm")
+user_dict = []
+nlp.tokenizer.pkuseg_update_user_dict(user_dict)
+
 
 def main():
-    nlp = spacy.load("zh_core_web_sm")
     doc = nlp(sentences[0])
     print(doc.text)
     for token in doc:
         print(token.text, token.pos_, token.dep_)
 
 
-if __name__ == "main":
+if __name__ == "__main__":
     main()
